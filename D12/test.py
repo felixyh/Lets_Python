dff0602430b3e411ba5c862ceef44ab286c7b212
felixyh <felix_yanghui@sina.cn>
2018-10-27 14:31:53 +0800

    initial

--- D12/test.py
+++ D12/test.py
@@ -3,21 +3,25 @@
 # print("hello, world!")
 
 
-tu = (1, 2)
-v1 = tu[0]
-
+# tu = (1, 2)
+# v1 = tu[0]
+#
 info = {
     1: "v1",
     "K2": "v2",
     # True: "123"
     "k3": (11, 22),
 }
-for k, v in info.items():
-    print(k, v)
+# for k, v in info.items():
+#     print(k, v)
+#
+# print(info.items())
+# print(info.keys())
+# print(info.values())
 
-print(info.items())
-print(info.keys())
-print(info.values())
+v = dict.fromkeys(["k1", "k2", "k3"], [1, 2, 3])
+print(v)
+
 
 
 

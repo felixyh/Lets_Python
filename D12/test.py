b45d346c6365fbee1fc44c5cc839c6c0cdd73cc6
felixyh <felix_yanghui@sina.cn>
2018-10-27 14:09:06 +0800

    initial

--- D12/test.py
+++ D12/test.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-print("hello, world!")
+# print("hello, world!")
+
+
+tu = (1, 2)
+v1 = tu[0]
+
+info = {
+    1: "v1",
+    "K2": "v2",
+    # True: "123"
+    "k3": (11, 22),
+}
+for k, v in info.items():
+    print(k, v)
+
+print(info.items())
+print(info.keys())
+print(info.values())
+
+
+
+
